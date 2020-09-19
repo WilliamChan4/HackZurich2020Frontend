8f7fd4296db378e20cb50f4fe96b074828751053
William Chan <william.chan4@gmail.com>
2020-09-20 00:55:26 +1000

    Method choice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.png';
-import './App.css';
+// import './App.css';
+import 'fontsource-roboto';
+import { Method } from './Method.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Method></Method>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/Method.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Grid, Paper, Container, Card, CardContent, Button, Typography, CardActions } from "@material-ui/core";
+
+export function Method() {
+    return (
+        <Container maxWidth="sm">
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">Pick Up</Typography>
+                            <Typography variant="body2" component="p">500m</Typography>
+                            <Typography variant="body2" component="p">15 minutes walk</Typography>
+                            <Typography variant="body2" component="p">5 minutes bike</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" color="primary">Choose</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">Delivery</Typography>
+                            <Typography variant="body2" component="p">Expected 15 minutes delivery time</Typography>
+                            <Typography variant="body2" component="p">Offset CO2 emissions for 20c</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" color="primary">Choose</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
